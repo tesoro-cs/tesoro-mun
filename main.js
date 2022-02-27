8d13c30fdc3e2c875613fa5f3134e083698839f5
ethank2222 <ethank2222@gmail.com>
2022-02-27 07:09:28 -0800

    navbar on small screen

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.setInterval(function() {
-    if(window.scrollY > 10){
+    if(window.scrollY > 10 || document.getElementById("menu-small") != null){
         if (document.getElementById("navigation").className.includes("colored") == false) {
             document.getElementById("navigation").classList.add("colored");
         }
@@ -28,3 +28,22 @@ for (var i = 0; i < x.length; i++){
         }
     });
 }
+document.getElementById("hamburger").addEventListener("click", function(){
+    if (document.getElementById("menu-small") == null){
+        var nav = document.getElementById("navigation");
+        var hamburger = document.getElementById("hamburger");
+        nav.classList.add("colored");
+        hamburger.className = "fa fa-close menu-item";
+        el = document.createElement("div");
+        el.id="menu-small";
+        el.innerHTML = hamburger.parentNode.innerHTML;
+        el.lastChild.remove();
+        el.lastChild.remove();
+        document.getElementById("navigation").parentNode.appendChild(el);
+    }
+    else{
+        var hamburger = document.getElementById("hamburger");
+        document.getElementById("menu-small").remove();
+        hamburger.className = "fa fa-bars menu-item";
+    }
+});
